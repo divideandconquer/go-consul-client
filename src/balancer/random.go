4a7d537a849e3afb427dbaf286bd766073e6ca15
Kyle Boorky <kyle.boorky@gmail.com>
2015-12-08 23:20:04 -0500

    adding initial dns load balancer

---
+++ src/balancer/random.go
@@ -0,0 +1,111 @@
+package balancer
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	"github.com/hashicorp/consul/api"
+)
+
+type cachedServiceLocation struct {
+	Services []ServiceLocation
+	CachedAt time.Time
+}
+
+type randomBalancer struct {
+	environment   string
+	consulCatalog *api.Catalog
+	cache         map[string]cachedServiceLocation
+	cacheLock     sync.RWMutex
+	ttl           time.Duration
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func NewRandomDNSBalancer(environment string, consulAddr string, cacheTTL time.Duration) (DNS, error) {
+	config := api.DefaultConfig()
+	config.Address = consulAddr
+	consul, err := api.NewClient(config)
+	if err != nil {
+		return nil, fmt.Errorf("Could not connect to consul: %v", err)
+	}
+
+	r := randomBalancer{}
+	r.cache = make(map[string]cachedServiceLocation)
+	r.environment = environment
+	r.ttl = cacheTTL
+	r.consulCatalog = consul.Catalog()
+	return &r, nil
+}
+
+func (r *randomBalancer) FindService(serviceName string) (ServiceLocation, error) {
+	services, err := r.getServiceFromCache(serviceName)
+	if err != nil || len(services) == 0 {
+		services, err = r.writeServiceToCache(serviceName)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return r.pickService(services), nil
+}
+
+func (r *randomBalancer) pickService(services []ServiceLocation) ServiceLocation {
+	return services[rand.Intn(len(services))]
+}
+
+func (r *randomBalancer) getServiceFromCache(serviceName string) ([]ServiceLocation, error) {
+	r.cacheLock.RLock()
+	defer r.cacheLock.RUnlock()
+
+	if result, ok := r.cache[serviceName]; ok {
+		if time.Now().UTC().Before(result.cachedAt.Add(r.ttl)) {
+			return result.Services, nil
+		}
+		return nil, fmt.Errorf("Cache for %s is expired", serviceName)
+	}
+	return nil, fmt.Errorf("Could not find %s in cache", serviceName)
+}
+
+// writeServiceToCache locks specifically to alleviate load on consul some additional lock time
+// is preferable to extra consul calls
+func (r *randomBalancer) writeServiceToCache(serviceName string) ([]ServiceLocation, error) {
+	//acquire a write lock
+	r.cacheLock.Lock()
+	defer r.cacheLock.Unlock()
+
+	//check the cache again in case we've fetched since the last check
+	//(our lock could have been waiting for another call to this function)
+	if result, ok := r.cache[serviceName]; ok {
+		if time.Now().UTC().Before(result.cachedAt.Add(r.ttl)) {
+			return r.pickService(result.Services), nil
+		}
+	}
+
+	//it still isn't in the cache, lets put it there
+	consulServices, _, err := r.consulCatalog.Service(serviceName, r.environment, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error reaching consul for service lookup %v", err)
+	}
+
+	if len(consulServices) == 0 {
+		return nil, fmt.Errorf("No services found for %s", serviceName)
+	}
+
+	//setup service locations
+	var services []ServiceLocation
+	for _, v := range consulServices {
+		s := ServiceLocation{}
+		s.URL = v.Address
+		s.Port = v.ServicePort
+		services = append(services, s)
+	}
+
+	// cache
+	c := cachedServiceLocation{Services: services, CachedAt: time.Now().UTC()}
+	r.cache[serviceName] = c
+	return services, nil
+}
